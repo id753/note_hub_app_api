7902aa03b35cc541dc755353cd199fe7a0d3df1c
Eugene O. (id753) <mimywhy@gmail.com>
2026-03-20 18:57:25 +0200

    Fix Session

--- src/services/auth.js
+++ src/services/auth.js
@@ -15,25 +15,27 @@ export const createSession = (userId) => {
   });
 };
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export const setSessionCookies = (res, session) => {
-  res.cookie('accessToken', session.accessToken, {
+  const cookieOptions = {
     httpOnly: true,
-    secure: true,
-    sameSite: 'none',
+    secure: isProd, // secure только в проде
+    sameSite: isProd ? 'none' : 'lax',
+  };
+
+  res.cookie('accessToken', session.accessToken, {
+    ...cookieOptions,
     maxAge: FIFTEEN_MINUTES,
   });
 
   res.cookie('refreshToken', session.refreshToken, {
-    httpOnly: true,
-    secure: true,
-    sameSite: 'none',
+    ...cookieOptions,
     maxAge: ONE_DAY,
   });
 
   res.cookie('sessionId', session._id, {
-    httpOnly: true,
-    secure: true,
-    sameSite: 'none',
+    ...cookieOptions,
     maxAge: ONE_DAY,
   });
 };
